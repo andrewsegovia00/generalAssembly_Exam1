12fbdad7f58d743e66a74fb829adb6f668f3426d
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-06-26 15:27:17 -0700

    Add negative class Add new logic to check if negative Add parseInt Input value is now 1

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-/* Cached Elements */
+/*              Cached Elements         */
 const addbtn = document.getElementById(`addBtn`);
 const subtBtn = document.getElementById(`subtBtn`);
 const userInput = document.getElementById(`userInput`);
 const currentResult = document.getElementById(`currentRes`);
 
-/* Event Listeners */
+/*              Event Listeners          */
 addbtn.addEventListener(`click`, handleClick);
 subtBtn.addEventListener(`click`, handleClick);
+ 
+/*              Functions                */
+function checkNegative() {
+    const currentResContent = parseInt(currentResult.innerHTML);
+    if(currentResContent < 0)
+    {
+        currentResult.classList.add(`negative`);
+    }
+    else
+    {
+        currentResult.classList.remove(`negative`);
+    }
+}
 
-/* Functions */
 function handleClick(event) {
-    const currentResContent = currentResult.innerHTML;
+    const currentResContent = parseInt(currentResult.innerHTML);
+    const userInputVal = parseInt(userInput.value);
 
     if (event.target.id === 'addBtn') 
     {
-        console.log(currentResContent);
+        currentResult.textContent = currentResContent + userInputVal;
     } 
     else if (event.target.id === 'subtBtn') 
     {
-        console.log(currentResContent);
+        currentResult.textContent = currentResContent - userInputVal;
     }
+
+    checkNegative();
+
 }
